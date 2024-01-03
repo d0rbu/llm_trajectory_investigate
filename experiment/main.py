9c9234080ac42453ebcd8bd08b1a6f8e1f27b6d5
d0rb <hacperu2010@gmail.com>
2024-01-03 17:47:54 -0600

    add get_full_trajectory fn header

--- experiment/main.py
+++ experiment/main.py
@@ -1,23 +1,34 @@
 import os
 import yaml
 import torch as th
+import torch.nn as nn
 from core.prompts import get_prompts
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from typing import Iterable
+from typing import Iterable, Mapping
 
+
+def get_full_trajectory(
+    model: nn.Module,
+    tokenizer,
+    prompt: Mapping[str, str | Iterable]
+) -> th.Tensor:
+    pass
 
 def get_full_trajectories(
     model_name: str = "meta-llama/Llama-2-7b",
     output_dir: os.PathLike | str | None = None,
     return_trajs: bool = True,
     prompt_types: Iterable[str] = [],
-) -> th.Tensor | None:
+) -> Iterable[Mapping] | None:
     assert return_trajs or output_dir, "where to return the trajectories? please specify return_trajs or output_dir"
 
     model = AutoModelForCausalLM.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
     prompts = get_prompts(prompt_types)
+
+    trajectories = [get_full_trajectory(model, tokenizer, prompt) for prompt in prompts]
+
 
 CONFIG_DIR = os.path.join("experiment", "configs")
 
