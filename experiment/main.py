d4cdc3b2c9812e446de45ad7cf9dfa810dd8487b
d0rb <hacperu2010@gmail.com>
2024-01-04 14:07:44 -0600

    pickle/torch save/load files rather than json

--- experiment/main.py
+++ experiment/main.py
@@ -2,7 +2,6 @@ import os
 import json
 import yaml
 import torch as th
-import torch.nn as nn
 from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedTokenizer, PreTrainedModel
 from hashlib import md5
 from typing import Iterable, Mapping
@@ -43,9 +42,8 @@ def get_model_trajectories(
                     **prompt,
                 } for prompt, trajectory in zip(prompts, trajectories)
             ])
-    
-    return model_trajectories
 
+    return model_trajectories
 
 def get_trajectories(
     model_names: Iterable[str] = ["meta-llama/Llama-2-7b"],
@@ -56,7 +54,7 @@ def get_trajectories(
 ) -> Iterable[Mapping]:
     prompts = get_prompts(prompt_types)
     prompts_hash = int(md5(str.encode(json.dumps(prompts, sort_keys=True))).hexdigest(), 16)
-    output_paths = [os.path.join(output_dir, model_name, prompts_hash, f"{model_name}.json") for model_name in model_names]
+    output_paths = [os.path.join(output_dir, model_name, prompts_hash, f"{model_name}.pt") for model_name in model_names]
     all_trajectories = {}
 
     for model_name, output_path in zip(model_names, output_paths):
@@ -64,11 +62,11 @@ def get_trajectories(
             try:
                 print("Found cached trajectory, loading...")
                 with open(output_path, "r", encoding="utf-8") as output_file:
-                    all_trajectories[model_name] = json.load(output_file)
+                    all_trajectories[model_name] = th.load(output_file)
 
                 continue
-            except json.JSONDecodeError:
-                print("Issue loading cached trajectory")
+            except Exception as e:
+                print(f"Issue loading cached trajectory: {e}")
 
         print("Loading model and tokenizer")
         model = AutoModelForCausalLM.from_pretrained(model_name)
@@ -85,13 +83,12 @@ def get_trajectories(
 
         if output_dir:
             with open(output_path, "w", encoding="utf-8") as output_file:
-                json.dump(model_trajectories, output_file)
+                th.save(model_trajectories, output_file)
 
     return all_trajectories
 
 
 CONFIG_DIR = os.path.join("experiment", "configs")
-
 
 if __name__ == "__main__":
     experiment_config_path = os.path.join(CONFIG_DIR, "main.yaml")
