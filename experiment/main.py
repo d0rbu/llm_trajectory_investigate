1cf79ba9a4a329eef06f023d6b13afe170aa717f
d0rb <hacperu2010@gmail.com>
2024-01-03 23:42:44 -0600

    finished get_trajectories no longer uses get_trajectory so that we can batch

--- experiment/main.py
+++ experiment/main.py
@@ -3,36 +3,51 @@ import json
 import yaml
 import torch as th
 import torch.nn as nn
-from core.prompts import get_prompts
 from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedTokenizer, PreTrainedModel
 from hashlib import md5
 from typing import Iterable, Mapping
+from more_itertools import batched
+from core.prompts import get_prompts
 
 
-def get_full_trajectory(
-    model: PreTrainedModel,
-    tokenizer: PreTrainedTokenizer,
-    prompt: Mapping[str, str | Iterable]
-) -> th.Tensor:
-    pass
-
-def get_full_trajectories(
+def get_trajectories(
     model_name: str = "meta-llama/Llama-2-7b",
     output_dir: os.PathLike | str | None = None,
     prompt_types: Iterable[str] = [],
+    batch_size: int = 16,
+    only_final_token: bool = True,
 ) -> Iterable[Mapping]:
     model = AutoModelForCausalLM.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
     prompts = get_prompts(prompt_types)
     prompts_hash = int(md5(str.encode(json.dumps(prompts, sort_keys=True))).hexdigest(), 16)
+    expected_outputs = [tokenizer.encode(prompt["answer"])[0] for prompt in prompts]
+    batched_prompts = batched(prompts, batch_size)
+    batched_expected_outputs = batched(expected_outputs, batch_size)
+    all_trajectories = []
 
-    trajectories = [
-        {
-            "trajectory": get_full_trajectory(model, tokenizer, prompt),
-            "prompt": prompt,
-        } for prompt in prompts
-    ]
+    with th.no_grad():
+        for prompts, expected_outputs in zip(batched_prompts, batched_expected_outputs):
+            prepared_prompts = tokenizer([prompt["prompt"] for prompt in prompts])
+
+            model_output = model(prepared_prompts, output_hidden_states = True)
+            chosen_outputs = th.argmax(model_output.logits[:, -1], dim=-1)
+            correct = chosen_outputs == th.tensor(expected_outputs)
+
+            if only_final_token:
+                trajectories = [hidden_state[:, :-1] for hidden_state in model_output.hidden_states]  # (L, B, D)
+            else:
+                trajectories = model_output.hidden_states  # (L, B, T, D)
+
+            trajectories = th.stack(trajectories, dim = 1)  # (B, L, [T], D)
+            all_trajectories.extend([
+                {
+                    "trajectory": trajectory,
+                    "correct": correct,
+                    **prompt,
+                } for prompt, trajectory in zip(prompts, trajectories)
+            ])
 
     if output_dir:
         output_path = os.path.join(output_dir, model_name, prompts_hash)
@@ -53,4 +68,4 @@ if __name__ == "__main__":
     else:
         experiment_config = {}
 
-    get_full_trajectories(output_dir="output", **experiment_config)
+    get_trajectories(output_dir="outputs", **experiment_config)
